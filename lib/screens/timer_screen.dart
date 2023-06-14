367046ad10709e707cc8d624956e17e7f99d8a5c
denos <costelasdenis@gmail.com>
2023-06-14 12:41:33 +0300

    made timer UI

--- lib/screens/timer_screen.dart
+++ lib/screens/timer_screen.dart
@@ -23,14 +23,15 @@ class _TimerScreenState extends ConsumerState<TimerScreen> {
   void initState() {
     super.initState();
 
-    _secondsController = FixedExtentScrollController();
-    _minutesController = FixedExtentScrollController();
-    _hoursController = FixedExtentScrollController();
+    _secondsController = FixedExtentScrollController(initialItem: 0);
+    _minutesController = FixedExtentScrollController(initialItem: 30);
+    _hoursController = FixedExtentScrollController(initialItem: 0);
   }
 
   @override
   Widget build(BuildContext context) {
     final width = MediaQuery.of(context).size.width;
+    final height = MediaQuery.of(context).size.height;
 
     final ColorScheme colors = Theme.of(context).colorScheme;
 
@@ -41,71 +42,128 @@ class _TimerScreenState extends ConsumerState<TimerScreen> {
         iconTheme: IconThemeData(color: colors.secondary),
       ),
       drawer: const CustomDrawer(),
-      body: Center(
-        child: Row(
-          children: [
-            // hours wheel
-            Container(
-              width: width * 0.2,
-              child: ListWheelScrollView.useDelegate(
-                controller: _hoursController,
-                itemExtent: 50,
-                perspective: 0.005,
-                diameterRatio: 1.2,
-                physics: const FixedExtentScrollPhysics(),
-                childDelegate: ListWheelChildBuilderDelegate(
-                  childCount: 24,
-                  builder: (context, index) {
-                    return HoursWidget(hours: index);
-                  },
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Center(
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: colors.tertiary,
+            ),
+            height: height * 0.5,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // row to SHOW TEXT hours / minutes / seconds
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Text(
+                        'Hours',
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: colors.secondary,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        'Minutes',
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: colors.secondary,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        'Seconds',
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: colors.secondary,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            ),
 
-            const SizedBox(
-              width: 10,
-            ),
+                // row for hours / minutes / seconds
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    // hours wheel
+                    SizedBox(
+                      height: height * 0.4,
+                      width: width * 0.2,
+                      child: ListWheelScrollView.useDelegate(
+                        controller: _hoursController,
+                        itemExtent: 50,
+                        perspective: 0.008,
+                        diameterRatio: 1.2,
+                        overAndUnderCenterOpacity: 0.3,
+                        physics: const FixedExtentScrollPhysics(),
+                        childDelegate: ListWheelChildBuilderDelegate(
+                          childCount: 24,
+                          builder: (context, index) {
+                            return HoursWidget(hours: index);
+                          },
+                        ),
+                      ),
+                    ),
 
-            // minutes wheel
-            Container(
-              width: width * 0.2,
-              child: ListWheelScrollView.useDelegate(
-                controller: _minutesController,
-                itemExtent: 50,
-                perspective: 0.005,
-                diameterRatio: 1.2,
-                physics: const FixedExtentScrollPhysics(),
-                childDelegate: ListWheelChildBuilderDelegate(
-                  childCount: 60,
-                  builder: (context, index) {
-                    return MinuntesWidget(minutes: index);
-                  },
+                    const SizedBox(
+                      width: 10,
+                    ),
+
+                    // minutes wheel
+                    SizedBox(
+                      height: height * 0.4,
+                      width: width * 0.2,
+                      child: ListWheelScrollView.useDelegate(
+                        controller: _minutesController,
+                        itemExtent: 50,
+                        perspective: 0.008,
+                        diameterRatio: 1.2,
+                        overAndUnderCenterOpacity: 0.3,
+                        physics: const FixedExtentScrollPhysics(),
+                        childDelegate: ListWheelChildBuilderDelegate(
+                          childCount: 60,
+                          builder: (context, index) {
+                            return MinuntesWidget(minutes: index);
+                          },
+                        ),
+                      ),
+                    ),
+
+                    const SizedBox(
+                      width: 15,
+                    ),
+
+                    //seconds wheel
+                    SizedBox(
+                      height: height * 0.4,
+                      width: width * 0.2,
+                      child: ListWheelScrollView.useDelegate(
+                        controller: _secondsController,
+                        itemExtent: 50,
+                        perspective: 0.008,
+                        diameterRatio: 1.2,
+                        overAndUnderCenterOpacity: 0.3,
+                        physics: const FixedExtentScrollPhysics(),
+                        childDelegate: ListWheelChildBuilderDelegate(
+                          childCount: 60,
+                          builder: (context, index) {
+                            return SecondsWidget(seconds: index);
+                          },
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-              ),
+              ],
             ),
-
-            const SizedBox(
-              width: 15,
-            ),
-
-            //seconds wheel
-            Container(
-              width: width * 0.2,
-              child: ListWheelScrollView.useDelegate(
-                controller: _secondsController,
-                itemExtent: 50,
-                perspective: 0.005,
-                diameterRatio: 1.2,
-                physics: const FixedExtentScrollPhysics(),
-                childDelegate: ListWheelChildBuilderDelegate(
-                  childCount: 60,
-                  builder: (context, index) {
-                    return SecondsWidget(seconds: index);
-                  },
-                ),
-              ),
-            ),
-          ],
+          ),
         ),
       ),
     );
